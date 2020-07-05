82abc9d879bbbaf7f63e5c1e08f5435b9c560a36
Golik_Andrii <Golik.Andrii@gmail.com>
2020-07-05 21:44:22 +0300

    new form

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('App is working...')
+import './styles.css'
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+
+function submitFormHandler(event) {
+    event.preventDefault()
+
+    console.log(input.value)
+}
